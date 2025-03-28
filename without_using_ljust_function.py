6bb6baf6e8dfa0b97ed5e933a69a83d43430efe3
unknown <carlosroque117@gmail.com>
2025-03-28 18:08:36 +0800

    Asks user for the word and the specified character.

--- without_using_ljust_function.py
+++ without_using_ljust_function.py
@@ -1,6 +1,9 @@
 #Ask the user for inputs.
+user_input = input("Enter a word: ")
+width = int(input("Enter the total width: "))
 
 #Adding space that completes the given number in the function parameter.
+
 
 #Print out the result.
 
