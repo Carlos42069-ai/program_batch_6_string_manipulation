626826c6ce71d9a0daabf5accce4b42b259a1886
unknown <carlosroque117@gmail.com>
2025-03-28 18:44:32 +0800

    Made a code that makes the first letter capital and all other letters in lower case without using capitalize function.

--- without_using_capitalize_funtion.py
+++ without_using_capitalize_funtion.py
@@ -1,5 +1,7 @@
 #Ask user for input.
-
+user_input = input("Enter a word: ")
 #Converting the first letter inputed into a capital and all other letters in lower case.
+if user_input:
+    capitalized_result = user_input[0].upper() + user_input[1:].lower()
 
 #Print out the results.
