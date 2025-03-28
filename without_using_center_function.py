507b1810c0223a1a284b3cd592f76c5fb615b652
unknown <carlosroque117@gmail.com>
2025-03-28 18:21:08 +0800

    Asks for a word and the specific characters that is needed.

--- without_using_center_function.py
+++ without_using_center_function.py
@@ -1,5 +1,6 @@
 #Asks user for the specific inputs.
-
+user_input = input("Enter a word: ")
+width = int(input("Enter the total width: "))
 #Calculate the padding or extra spaces to create a centered string.
 
 #Print out the result.
