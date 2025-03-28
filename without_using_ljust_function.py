c4f55202402844784363572bfe6dea00041ff276
unknown <carlosroque117@gmail.com>
2025-03-28 18:11:38 +0800

    Added a code that adds a space to complete the given number of characters.

--- without_using_ljust_function.py
+++ without_using_ljust_function.py
@@ -3,7 +3,7 @@ user_input = input("Enter a word: ")
 width = int(input("Enter the total width: "))
 
 #Adding space that completes the given number in the function parameter.
-
+padded_result = user_input + " " * (width - len(user_input)) if len(user_input) < width else user_input
 
 #Print out the result.
 
