cad2041ae7f0392c053412bc805b4059a137a8c6
unknown <carlosroque117@gmail.com>
2025-03-28 19:23:28 +0800

    Prints out the result. With this program finished, I have successfully finished all the programs in batch 6.

--- without_using_title_function.py
+++ without_using_title_function.py
@@ -3,3 +3,4 @@ user_input = input("Enter a sentence: ")
 #Creating the first letter of each word capital without using title().
 title_case_result = " ".join(word.capitalize() for word in user_input.split())
 #Print out the result. 
+print("The result is:", title_case_result)
