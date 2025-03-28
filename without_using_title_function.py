8ae6904c0173481a2cb4774fa4d9836bac048a6f
unknown <carlosroque117@gmail.com>
2025-03-28 19:20:57 +0800

    Made a code that asks a sentence and setting conditions in order to capitalize each first letter in a word from the given sentence without using the title function.

--- without_using_title_function.py
+++ without_using_title_function.py
@@ -1,5 +1,5 @@
 #Ask the user for a complete sentence.
-
+user_input = input("Enter a sentence: ")
 #Creating the first letter of each word capital without using title().
-
+title_case_result = " ".join(word.capitalize() for word in user_input.split())
 #Print out the result. 
