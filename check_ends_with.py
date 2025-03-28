95872d539bc6a9de4805134009511b686fcd9c22
unknown <carlosroque117@gmail.com>
2025-03-28 17:51:11 +0800

    Asks the user for a word and a suffix to be checked, then made a code that checks if the suffix is within the inputed word.

--- check_ends_with.py
+++ check_ends_with.py
@@ -1,5 +1,7 @@
 #Ask the user for input and suffix to check if the word ends with the given suffix.
-
+ask_user = input("Enter a word with a suffix: ")
+suffix_input = input("Enter a suffix to check: ")
 #Check if the suffix inputed is within the given word.
+check_suffix = ask_user[-len(suffix_input):] == suffix_input if len(suffix_input) <= len(ask_user) else False
 
 #Print if it is true or false.
