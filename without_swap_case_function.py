93990323c2c651c3b7c5b4ee291fe4dcd22908f7
unknown <carlosroque117@gmail.com>
2025-03-28 18:34:41 +0800

    Made a code that swaps the cases without using swapcase function.

--- without_swap_case_function.py
+++ without_swap_case_function.py
@@ -1,5 +1,6 @@
 #Asks user for input.
-
+user_input = input("Enter a word: ")
 #Create a code that has the same function with swapcase without using the function.
+swapped_result = "".join(chr(ord(c) ^ 32) if c.isalpha() else c for c in user_input)
 
 #Print out the results.
