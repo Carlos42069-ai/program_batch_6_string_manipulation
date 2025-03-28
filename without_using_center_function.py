7e0f12810b761e8f7cfdcd857b248ca521796e88
unknown <carlosroque117@gmail.com>
2025-03-28 18:24:17 +0800

    Made a code that centers the word within the given amount of width.

--- without_using_center_function.py
+++ without_using_center_function.py
@@ -2,5 +2,6 @@
 user_input = input("Enter a word: ")
 width = int(input("Enter the total width: "))
 #Calculate the padding or extra spaces to create a centered string.
-
+padding = max(0, width - len(user_input))
+padded_result = " " * (padding // 2) + user_input + " " * (padding - padding // 2)
 #Print out the result.
