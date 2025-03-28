1e32f05ffea1f669ff764f1949f50337d039d3b3
unknown <carlosroque117@gmail.com>
2025-03-28 16:57:30 +0800

    Added a code that removes the prefix from the input.

--- without_using_remove_prefix.py
+++ without_using_remove_prefix.py
@@ -3,5 +3,6 @@ ask_user = input("Enter a word: ")
 add_prefix = input("Enter the prefix that you want to remove: ")
 
 #Remove the prefix from the input.
-
+if ask_user .startswith(add_prefix):
+    ask_user = ask_user[len(prefix):] 
 #Print the new output. 
