49c2fce6e36c0f16ca906beebc09f17b83bbb324
unknown <carlosroque117@gmail.com>
2025-03-28 17:23:07 +0800

    Asks user for input and made a code that lowers the characters inputed.

--- without_using_lower_function.py
+++ without_using_lower_function.py
@@ -1,6 +1,12 @@
 #Ask user for an input.
-
+ask_user = ("Enter a word: ")
 #Code that lowers the characters without using lower().
+lowercase_output = ""
+for character in user_input:
+    if 'A' <= character <= 'Z':
+        lowercase_result += chr(ord(character) + 32)
+    else:
+        lowercase_result += character
 
 
 #Print the output.
