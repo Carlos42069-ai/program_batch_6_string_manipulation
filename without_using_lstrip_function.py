de7b2e5dcebabb975963e90b165019a8d81be910
unknown <carlosroque117@gmail.com>
2025-03-28 16:41:48 +0800

    Added a code that prints the new output.

--- without_using_lstrip_function.py
+++ without_using_lstrip_function.py
@@ -4,7 +4,8 @@ ask_user = input("Enter a word with spaces in the beginning: ")
 #Code that removes the spaces without using the lstrip function.
 space = 0
 
-while space < len(ask_user) and ask_user[space] == ' '
+while space < len(ask_user) and ask_user[space] == ' ':
     space += 1
 
 #print the output.
+print("The output now is:", (ask_user[space:]))
