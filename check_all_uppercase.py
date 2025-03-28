8f6f9ea0d357588363c0b778950b4c99ebb30f30
unknown <carlosroque117@gmail.com>
2025-03-28 17:37:38 +0800

    The program asks user for input then made a code that checks if the input is all in upper cases then results is true, and if a lower case is found then the result is false.

--- check_all_uppercase.py
+++ check_all_uppercase.py
@@ -1,5 +1,11 @@
 #Ask user for input.
+ask_user = input("Enter a word in upper cases: ")
 
 #Check if the input is all in upper cases.
+all_upper_cases = True
+for character in ask_user:
+    if 'a' <= character <= 'z':  
+        all_upper_cases = False
+        break
 
 #Print the result whether it's true or false.
